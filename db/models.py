d5034644512c26b014b108795dc79c683e527f6b
Ihor Borunov <borunov65@gmail.com>
2025-06-13 20:53:57 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -8,8 +8,6 @@ from django.core.exceptions import ValidationError
 
 from django.conf import settings
 
-from django.utils import timezone
-
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -68,7 +66,7 @@ class MovieSession(models.Model):
 
 
 class Order(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
